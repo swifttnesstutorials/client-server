5fd00a723dfc555401e7f04518c3d3d3099113c4
ARYANANDHA <aryanandhaaryanandha5@gmail.com>
2024-11-15 15:33:48 +0530

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middlewares/authMiddlewares.js');
-const { getUserProfile, userSignup, userLogin, updateUser, deleteUser } = require('../controllers/usercontrollers.js');
+const { getUserProfile, userSignup, userLogin, updateUser, deleteUser } = require('../controllers/userControllers.js');  // Corrected import path
 
+// Routes
 router.post('/signup', userSignup);
 router.post('/login', userLogin);
 
 // Profile route for authenticated users
-router.get('/profile', verifyToken, getUserProfile); // New profile route
+router.get('/profile', verifyToken, getUserProfile);
 
 router.patch('/:userId', verifyToken, updateUser);
 router.delete('/:userId', verifyToken, deleteUser);
